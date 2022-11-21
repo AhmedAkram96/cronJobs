41590fd65a4b269a4a7bfb069e9d8100daf31bf3
ahmedakram96 <ahmed.akram32765@gmail.com>
2022-11-21 15:11:51 +0200

    remove not needed attr

--- config/index.js
+++ config/index.js
@@ -3,11 +3,10 @@ module.exports = {
     receiveMoney: {
         frequency: "*/2 * * * * *",
         handler: "handlers/receiveMoney",
-        singleRun: true
     },
     //Example for a job that's scheduled for single run at exact time
     sendMoney: {
-        frequency: "30 3 2 20 11 *",
+        frequency: "30 3 2 20 11 5",
         handler: "handlers/sendMoney"
     }
 }
